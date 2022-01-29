60aa6c565770940c5da882457a9f234f9c00a7db
josip <josip.mucak@bornfight.com>
2022-01-29 19:55:11 +0100

    update chart data

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -3,6 +3,7 @@ import './Expenses.css';
 import {ExpensesFilter} from "./ExpensesFilter";
 import {useState} from "react";
 import {ExpensesList} from "./ExpensesList";
+import {ExpensesChart} from "./ExpensesChart";
 
 export const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -18,6 +19,7 @@ export const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}/>
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
